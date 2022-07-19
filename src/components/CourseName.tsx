1c49d65176198864908fc104e8d39db667ce581c
Hscomp <hscomp2002@gmail.com>
2022-07-19 17:08:38 +0430

    chnage select to Autocomplete

--- src/components/CourseName.tsx
+++ src/components/CourseName.tsx
@@ -2,12 +2,17 @@ import { typesObject } from 'constants/index';
 
 const CourseName = ({ course }: any) => {
     return (<>
-        {course.lesson?.name} -
+        {course?.lesson?.name} -
         {course.teacher.first_name} {course.teacher.last_name} -
         {course.name} -
         {typesObject[course.type]} -
         مقطع {course.education_level}
     </>)
 }
+
+export const getCourseName = (course: any): string => {
+    return `${course?.lesson?.name} - ${course.teacher.first_name} ${course.teacher.last_name} - ${course.name} - ${typesObject[course.type]} - مقطع ${course.education_level}`
+}
+
 
 export default CourseName;
--- src/screens/Students/StudentCourses/components/AddStudentCourse.tsx
+++ src/screens/Students/StudentCourses/components/AddStudentCourse.tsx
@@ -1,10 +1,12 @@
-import { Box, Button, FormControl, MenuItem, Paper, Typography } from "@mui/material";
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Box, Button, FormControl, Paper, Typography } from "@mui/material";
+// import Select, { SelectChangeEvent } from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import { GET_COURSES } from '../gql/query';
 import { useQuery, useMutation } from "@apollo/client";
 import { CREATE_STUDENT_COURSE } from '../gql/mutation';
-import { useState } from "react";
-import CourseName from "components/CourseName";
+import { useEffect, useState } from "react";
+import { getCourseName } from "components/CourseName";
 import { showSuccess } from "utils/swlAlert";
 
 interface Props {
@@ -14,7 +16,10 @@ interface Props {
 
 const AddStudentCourse = ({ studentId, refetch }: Props) => {
     const [courseId, setCourseId] = useState<string | undefined>("");
-    const { data: coursesData, loading } = useQuery(GET_COURSES, {
+    const [courseOptions, setCourseOptions] = useState<any[]>([
+        { label: '', id: "" },
+    ]);
+    const { data: coursesData } = useQuery(GET_COURSES, {
         variables: {
             first: 1000,
             page: 1,
@@ -32,7 +37,7 @@ const AddStudentCourse = ({ studentId, refetch }: Props) => {
         const input = {
             variables: {
                 student_id: studentId ? parseInt(studentId) : 0,
-                course_id: courseId ? parseInt(courseId) : 0,
+                course_id: courseId ? parseInt(courseId) : null,
                 status: "ok"
             }
         }
@@ -42,9 +47,31 @@ const AddStudentCourse = ({ studentId, refetch }: Props) => {
         });
     }
 
-    const handleChangeCourse = (e: SelectChangeEvent<string>) => {
-        setCourseId(e.target.value)
+    // const handleChangeCourse = (e: SelectChangeEvent<string>) => {
+    //     setCourseId(e.target.value)
+    // }
+
+    useEffect(() => {
+        if (coursesData?.getCourses?.data) {
+            const tmp = [
+                { label: '', id: 0 },
+            ];
+            setCourseOptions(tmp);
+            for (const i in coursesData.getCourses.data) {
+                const course = coursesData.getCourses.data[i];
+                tmp.push({
+                    id: +course.id,
+                    label: getCourseName(course)
+                })
+            }
+
+        }
+
+
     }
+        , [coursesData]);
+
+
     return (<Box
         component={Paper}
         sx={{ p: 1, my: 2 }}
@@ -54,7 +81,7 @@ const AddStudentCourse = ({ studentId, refetch }: Props) => {
         </Typography>
         <Box>
             <FormControl sx={{ width: 500 }}>
-                <Select
+                {/* <Select
                     defaultValue=""
                     value={courseId}
                     onChange={handleChangeCourse}
@@ -73,7 +100,18 @@ const AddStudentCourse = ({ studentId, refetch }: Props) => {
                         </MenuItem>
                     ))
                     }
-                </Select>
+                </Select> */}
+                {courseOptions.length ? <Autocomplete
+                    onChange={(event: any, newValue: any) => {
+                        setCourseId(newValue?.id);
+                    }}
+                    disablePortal
+                    id="combo-box-demo"
+                    options={courseOptions}
+                    sx={{ width: 500 }}
+                    renderInput={(params) => <TextField {...params} label="انتخاب درس" />}
+                /> : null}
+
             </FormControl>
             <Button
                 variant="contained"
