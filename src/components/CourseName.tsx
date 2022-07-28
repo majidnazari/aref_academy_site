44f36f0e71b8aec2ed0770d8d958cc459139d9f5
Hscomp <hscomp2002@gmail.com>
2022-07-28 10:58:53 +0430

    bug Fixed

--- src/components/CourseName.tsx
+++ src/components/CourseName.tsx
@@ -3,10 +3,10 @@ import { typesObject } from 'constants/index';
 const CourseName = ({ course }: any) => {
     return (<>
         {course?.lesson?.name} -
-        {course.teacher.first_name} {course.teacher.last_name} -
-        {course.name} -
-        {typesObject[course.type]} -
-        مقطع {course.education_level}
+        {course?.teacher.first_name} {course?.teacher.last_name} -
+        {course?.name} -
+        {typesObject[course?.type]} -
+        مقطع {course?.education_level}
     </>)
 }
 
