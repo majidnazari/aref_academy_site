e7650f1a563b92568481d4d7ceea40e5e82f16ed
Hscomp <hscomp2002@gmail.com>
2022-10-12 15:31:38 +0330

    bug fixed

--- src/screens/Students/StudentCourses/components/AddStudentCourse.tsx
+++ src/screens/Students/StudentCourses/components/AddStudentCourse.tsx
@@ -1,85 +1,85 @@
-import { Box, Button, FormControl, Paper, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, FormControl, Paper, Typography } from "@mui/material";
 // import Select, { SelectChangeEvent } from '@mui/material/Select';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import { GET_COURSES } from '../gql/query';
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { GET_COURSES } from "../gql/query";
 import { useQuery, useMutation } from "@apollo/client";
-import { CREATE_STUDENT_COURSE } from '../gql/mutation';
+import { CREATE_STUDENT_COURSE } from "../gql/mutation";
 import { useEffect, useState } from "react";
 import { getCourseName } from "components/CourseName";
 import { showSuccess } from "utils/swlAlert";
 
 interface Props {
-    studentId: string | undefined;
-    refetch: Function;
+  studentId: string | undefined;
+  refetch: Function;
 }
 
 const AddStudentCourse = ({ studentId, refetch }: Props) => {
-    const [courseId, setCourseId] = useState<string | undefined>("");
-    const [courseOptions, setCourseOptions] = useState<any[]>([
-        { label: '', id: "" },
-    ]);
-    const { data: coursesData } = useQuery(GET_COURSES, {
-        variables: {
-            first: 1000,
-            page: 1,
-            orderBy: [{
-                column: 'id',
-                order: 'DESC'
-            }],
-            fetchPolicy: "no-cache"
-        }
+  const [courseId, setCourseId] = useState<string | undefined>("");
+  const [courseOptions, setCourseOptions] = useState<any[]>([
+    { label: "", id: "" },
+  ]);
+  const { data: coursesData } = useQuery(GET_COURSES, {
+    variables: {
+      first: 1000,
+      page: 1,
+      orderBy: [
+        {
+          column: "id",
+          order: "DESC",
+        },
+      ],
+      fetchPolicy: "no-cache",
+    },
+  });
+
+  const [createStudentCourse, { loading: addStudentLoading }] = useMutation(
+    CREATE_STUDENT_COURSE
+  );
+
+  const insertStudentCourse = () => {
+    const input = {
+      variables: {
+        student_id: studentId ? parseInt(studentId) : 0,
+        course_id: courseId ? parseInt(courseId) : null,
+        status: "ok",
+      },
+    };
+    createStudentCourse(input).then(() => {
+      showSuccess("درس با موفقیت اضافه شد");
+      refetch();
     });
+  };
 
-    const [createStudentCourse] = useMutation(CREATE_STUDENT_COURSE);
+  // const handleChangeCourse = (e: SelectChangeEvent<string>) => {
+  //     setCourseId(e.target.value)
+  // }
 
-    const insertStudentCourse = () => {
-        const input = {
-            variables: {
-                student_id: studentId ? parseInt(studentId) : 0,
-                course_id: courseId ? parseInt(courseId) : null,
-                status: "ok"
-            }
-        }
-        createStudentCourse(input).then(() => {
-            showSuccess("درس با موفقیت اضافه شد");
-            refetch();
+  useEffect(() => {
+    if (coursesData?.getCourses?.data) {
+      const tmp = [{ label: "", id: 0 }];
+      setCourseOptions(tmp);
+      for (const i in coursesData.getCourses.data) {
+        const course = coursesData.getCourses.data[i];
+        tmp.push({
+          id: +course.id,
+          label: getCourseName(course),
         });
+      }
     }
+  }, [coursesData]);
 
-    // const handleChangeCourse = (e: SelectChangeEvent<string>) => {
-    //     setCourseId(e.target.value)
-    // }
-
-    useEffect(() => {
-        if (coursesData?.getCourses?.data) {
-            const tmp = [
-                { label: '', id: 0 },
-            ];
-            setCourseOptions(tmp);
-            for (const i in coursesData.getCourses.data) {
-                const course = coursesData.getCourses.data[i];
-                tmp.push({
-                    id: +course.id,
-                    label: getCourseName(course)
-                })
-            }
-
-        }
-    }
-        , [coursesData]);
-
-
-    return (<Box
-        component={Paper}
-        sx={{ p: 1, my: 2 }}
-    >
-        <Typography component={'div'} sx={{ fontSize: 18, fontWeight: 'bold', my: 2 }} >
-            افزودن درس
-        </Typography>
-        <Box>
-            <FormControl sx={{ width: 500 }}>
-                {/* <Select
+  return (
+    <Box component={Paper} sx={{ p: 1, my: 2 }}>
+      <Typography
+        component={"div"}
+        sx={{ fontSize: 18, fontWeight: "bold", my: 2 }}
+      >
+        افزودن درس
+      </Typography>
+      <Box>
+        <FormControl sx={{ width: 500 }}>
+          {/* <Select
                     defaultValue=""
                     value={courseId}
                     onChange={handleChangeCourse}
@@ -99,30 +99,40 @@ const AddStudentCourse = ({ studentId, refetch }: Props) => {
                     ))
                     }
                 </Select> */}
-                {courseOptions.length ? <Autocomplete
-                    onChange={(event: any, newValue: any) => {
-                        setCourseId(newValue?.id);
-                    }}
-                    disablePortal
-                    id="combo-box-demo"
-                    options={courseOptions}
-                    sx={{ width: 500 }}
-                    renderInput={(params) => <TextField {...params} label="انتخاب درس" />}
-                /> : null}
-
-            </FormControl>
-            <Button
-                variant="contained"
-                color="primary"
-                sx={{ mt: 2, mx: 1 }}
-                onClick={() => {
-                    insertStudentCourse();
-                }}
-            >
-                افزودن
-            </Button>
-        </Box>
-    </Box>)
-}
+          {courseOptions.length ? (
+            <Autocomplete
+              onChange={(event: any, newValue: any) => {
+                setCourseId(newValue?.id);
+              }}
+              disablePortal
+              id="combo-box-demo"
+              options={courseOptions}
+              sx={{ width: 500 }}
+              renderInput={(params) => (
+                <TextField {...params} label="انتخاب درس" />
+              )}
+            />
+          ) : null}
+        </FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2, mx: 1 }}
+          onClick={() => {
+            insertStudentCourse();
+          }}
+          disabled={addStudentLoading}
+          endIcon={
+            addStudentLoading ? (
+              <CircularProgress color="inherit" size={10} />
+            ) : null
+          }
+        >
+          افزودن
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
 export default AddStudentCourse;
