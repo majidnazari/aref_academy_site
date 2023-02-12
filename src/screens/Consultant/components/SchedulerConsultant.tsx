e319aa04fb73eac48c2976f3db280d2c5d1922a2
MAJID n <majidnazarister@gmail.com>
2023-02-12 22:42:46 +0330

    .

--- src/screens/Consultant/components/SchedulerConsultant.tsx
+++ src/screens/Consultant/components/SchedulerConsultant.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
@@ -53,14 +53,23 @@ interface ErrorData {
 }
 
 const SchedulerConsultant = ({ userId }: any) => {
-    userId = "63da17010eb3b6ec9e7f4ec7";
+    //userId = "63da17010eb3b6ec9e7f4ec7";
+    userId = "63d3dd302c419803bc1b6516";
+
     const navigate = useNavigate();
 
     const [consultant, setConsultant] = useState<ConsultantData | null>(null);
     const [step, setStep] = useState<number | null>(null);
     const [saturday, setSaturday] = useState<StartEnd[] | null>(null);
-    //const [saturdayTimeTable, setSaturdayTimeTable] = useState<any | null>(null);
-    let saturdayTimeTable: string[]=[];
+    const [saturdayTimeTableState, setSaturdayTimeTableState] = useState<any | null>(null);
+    const [sundayTimeTableState, setSundayTimeTableState] = useState<any | null>(null);
+    const [mondayTimeTableState, setMondayTimeTableState] = useState<any | null>(null);
+    const [thuesdayTimeTableState, setThuesdayTimeTableState] = useState<any | null>(null);
+    const [wednesdayTimeTableState, setWednesdayTimeTableState] = useState<any | null>(null);
+    const [thursdayTimeTableState, setThursdayTimeTableState] = useState<any | null>(null);
+    const [fridayTimeTableState, setFridayTimeTableState] = useState<any | null>(null);
+    let saturdayTimeTable: string[] = Array(144).fill("00:00-00:00");
+    let allSaturday: string[] = [];
     const [timetable, setTimetable] = useState<string[] | null>(null);
     let tmp: any = [];
     const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -80,42 +89,39 @@ const SchedulerConsultant = ({ userId }: any) => {
         onCompleted: (data) => {
             setConsultant(data.consultant);
             setStep(data.consultant.step);
-            const allTimeOfDay = getTimeTableTitle(data.consultant.step);
-            // console.log("allTimeOfDay is:" ,allTimeOfDay);
-            // console.log("timeTablesTitle");
-            // console.log({timeTablesTitle});
-            calculateSaturday(allTimeOfDay, data.consultant.timeTable);
+            const allTimeOfDay = getTimeTableTitle(data.consultant.step);           
+            allSaturday = calculateSaturday(allTimeOfDay, data.consultant.timeTable);           
+            setSaturdayTimeTableState(allSaturday);
+            //console.log("allSaturday is:", allSaturday);
 
         },
         fetchPolicy: "no-cache",
     });
 
     const getTimeTableTitle = (userStep: number) => {
-        let timeTableFrom:any ;
+        let timeTableFrom: any;
         let timeTableTo = "";
         let timeTable = [];
 
         let now = moment("08:00", 'HHmm').format("HH:mm");
         let date = new Date();
-        timeTableFrom = moment([date.getFullYear(), date.getMonth()+1, date.getDate(), 8, 0, 0]).format("HH:mm");
+        timeTableFrom = moment([date.getFullYear(), date.getMonth() + 1, date.getDate(), 8, 0, 0]).format("HH:mm");
         //timeTableFrom = moment([8,0,0],"hh:mm");
-        let timetmp = now.split(':'); 
+        let timetmp = now.split(':');
         for (let hour = 8; hour < 20;) {
 
-            timeTableTo = moment([date.getFullYear(), date.getMonth()+1, date.getDate(), timetmp[0], timetmp[1], 0]).add(5, 'minutes').format("HH:mm");
+            timeTableTo = moment([date.getFullYear(), date.getMonth() + 1, date.getDate(), timetmp[0], timetmp[1], 0]).add(5, 'minutes').format("HH:mm");
             timetmp = timeTableTo.split(':');
             timeTable.push(timeTableFrom + "-" + timeTableTo);
             timeTableFrom = timeTableTo;
             hour = parseInt(timetmp[0]);
         }
-        setTimetable(timeTable);        
+        setTimetable(timeTable);
         return timeTable;
 
     }
     const calculateSaturday = (timeTablesTitle: string[], userTimeTables: TimeTable[]) => {
-        const saturdays: any = [];
-
-        // console.log("userTimeTable.dayOfWeek",userTimeTables);       
+        const saturdays: any = [];    
         userTimeTables?.forEach(userTimeTable => {
             switch (userTimeTable.dayOfWeek) {
                 case "SATURDAY":
@@ -126,57 +132,46 @@ const SchedulerConsultant = ({ userId }: any) => {
 
                     break;
             }
-            setSaturday(saturdays);            
-            console.log("saturdays is:" ,saturdays);
+            setSaturday(saturdays);          
         });
 
-        let index = 0;
+
         let timetabletmp: string[] = [];
-        let timetabletmpstart:any;
-        let timetabletmpend:any;
+        let timetabletmpstart: any;
+        let timetabletmpend: any;
         let timetabletmpstarthour = "";
         let timetabletmpstartminute = "";
-       
+
         let timetabletmpendhour = "";
         let timetabletmpendminute = "";
-        let starttmp:any;
-        let endtmp :any;
+        let starttmp: any;
+        let endtmp: any;
         let startHour = "";
         let startMinute = "";
         let endHour = "";
         let endMinute = "";
-        const format='hh:mm';
+        const format = 'hh:mm';
 
         saturdays.forEach((saturdayElement: any) => {
-            starttmp=moment(saturdayElement.start.trim(),'hh:mm');
-            endtmp=moment(saturdayElement.end.trim(),'hh:mm');           
-
+            starttmp = moment(saturdayElement.start.trim(), 'hh:mm');
+            endtmp = moment(saturdayElement.end.trim(), 'hh:mm');
+            let index = 0;
             timeTablesTitle?.forEach(timeTableElement => {
                 //console.log("the timeTableElement foreach is:", timeTableElement);
                 timetabletmp = timeTableElement.split('-');
-                timetabletmpstart=moment(timetabletmp[0].trim(),'hh:mm');
-                timetabletmpend=moment(timetabletmp[1].trim(),'hh:mm');
-
-                console.log("the timetabletmpstart is:", timetabletmpstart);
-                console.log("the timetabletmpend is:", timetabletmpend);
-
-                console.log("the starttmp is:", starttmp);
-                console.log("the endtmp is:", endtmp);
-
-            //    let range = moment(timetabletmpstart,timetabletmpend).isBetween(starttmp, endtmp);
-            //    console.log("range is:" ,range);
-                if ((timetabletmpstart.isBetween(starttmp, endtmp)) || (timetabletmpend.isBetween(starttmp, endtmp))) {
-                //if (timetabletmpstart.isBetween(starttmp, endtmp)) {
-                    //setSaturdayTimeTable([...saturdayTimeTable,timeTableElement]);
-                   saturdayTimeTable.push(timeTableElement);
-                   // console.log("saturdayTimeTable is: ", saturdayTimeTable);
-                    //console.log(" the timeTableElement : ", timeTableElement, " is between of : " ,saturdayElement );
-                }
-
+                timetabletmpstart = moment(timetabletmp[0].trim(), 'hh:mm');
+                timetabletmpend = moment(timetabletmp[1].trim(), 'hh:mm');
+                if ((timetabletmpstart.isBetween(starttmp, endtmp)) || (timetabletmpend.isBetween(starttmp, endtmp))) {                  
+                    saturdayTimeTable[index] = timeTableElement;                   
+                }               
+                index++;
             });
         })
-        console.log("saturdayTimeTable is:",saturdayTimeTable);
+        return saturdayTimeTable;       
     }
+    useEffect(() => {
+       
+    }, [saturdayTimeTableState])
 
     const StyledTableRow = styled(TableRow)(({ theme }) => ({
         '&:nth-of-type(odd)': {
@@ -186,52 +181,60 @@ const SchedulerConsultant = ({ userId }: any) => {
             border: 0,
         },
     }));
-    return (<Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
-        <Typography component={'div'} sx={{ fontSize: 18, fontWeight: 'bold', my: 2 }} >
-            تایم های مشاور
-        </Typography>
-        <TableContainer component={Paper}>
-            <Table aria-label="customized table " style={{ lineHeight: "0.1px" }}>
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell align="left">ساعات حضور</StyledTableCell>
-                        <StyledTableCell align="left">شنبه</StyledTableCell>
-                        <StyledTableCell align="left"> یکشنبه </StyledTableCell>
-                        <StyledTableCell align="left"> دوشنبه  </StyledTableCell>
-                        <StyledTableCell align="left">  سه شنبه </StyledTableCell>
-                        <StyledTableCell align="left">چهارشنبه  </StyledTableCell>
-                        <StyledTableCell align="left">پنج شنبه</StyledTableCell>
-                        <StyledTableCell align="left"> جمعه </StyledTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody> 
-                           
-                    {
-                        timetable?.map((elementtmp: string, index: number) => (
-                            <StyledTableRow key={index++}>
-                                <StyledTableCell align="left">
-                                    {
-                                        elementtmp
-                                    }
-                                </StyledTableCell>                               
-                                {/* {elementtmp == saturdayTimeTable
-                                    ?
-                                    <StyledTableCell align="left" style={{ backgroundColor: 'blueviolet', color: 'black', }} />
-                                    :
-                                    <StyledTableCell align="left" />} */}
-                               
+    return (
+        <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
+            <Typography component={'div'} sx={{ fontSize: 18, fontWeight: 'bold', my: 2 }} >
+                تایم های مشاور
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table aria-label="customized table " >
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell align="left">ساعات حضور</StyledTableCell>
+                            <StyledTableCell align="left">شنبه</StyledTableCell>
+                            <StyledTableCell align="left"> یکشنبه </StyledTableCell>
+                            <StyledTableCell align="left"> دوشنبه  </StyledTableCell>
+                            <StyledTableCell align="left">  سه شنبه </StyledTableCell>
+                            <StyledTableCell align="left">چهارشنبه  </StyledTableCell>
+                            <StyledTableCell align="left">پنج شنبه</StyledTableCell>
+                            <StyledTableCell align="left"> جمعه </StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody >
+                        
+                        {
+                            saturdayTimeTableState
+                                ?
+                              
+                                timetable?.map((elementtmp: string, index: number) => (
+                                    <StyledTableRow key={index++}>
+                                        <StyledTableCell align="left">
+                                            {
+                                                elementtmp
+                                            }
+                                        </StyledTableCell>
+                                      
+                                        {
+                                            elementtmp == saturdayTimeTableState[index]
+                                                ?
+                                                <StyledTableCell align="left" style={{ backgroundColor: 'blueviolet', color: 'black', }} />
+                                                :
+                                                <StyledTableCell align="left" />
+                                        }
 
-                            </StyledTableRow>
-                        ))
 
-                    }
+                                    </StyledTableRow>
+                                ))
+                                :
+                                ""
+                        }
 
 
-                </TableBody>
-            </Table>
+                    </TableBody>
+                </Table>
 
-        </TableContainer>
-    </Container >);
+            </TableContainer>
+        </Container >);
 }
 
 export default SchedulerConsultant;
