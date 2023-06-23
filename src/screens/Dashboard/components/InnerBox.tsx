df084859027cdaa9d1d06d74c8074db97dfaed15
majid nazari <majidnazarister@gmail.com>
2023-06-23 11:00:24 +0330

    add icon ro report

--- src/screens/Dashboard/components/InnerBox.tsx
+++ src/screens/Dashboard/components/InnerBox.tsx
@@ -1,11 +1,13 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import CourseSessionDetailsType from "../dto/CourseSessionDetailsType";
-//import Face3OutlinedIcon from '@mui/icons-material/Face3Outlined';
+
+import femalepng from "../../../assets/img/female.png";
+import malepng from "../../../assets/img/male.png";
 
 const InnerBox = ({ details }: { details: CourseSessionDetailsType[] }) => {
-  const innerBox = {
-    backgroundColor: "purple",
+  const innerBoxEducationUnder12 = {
+    backgroundColor: "#02acf5",
     color: "white",
     borderRadius: "5px",
     boxShadow: 3,
@@ -17,44 +19,77 @@ const InnerBox = ({ details }: { details: CourseSessionDetailsType[] }) => {
     width: 150,
     m: 1,
     direction: "rtl",
-    flexDirection:"column",
-    
+    flexDirection: "column",
   };
 
-  const smallBox = {
-    backgroundColor: "green",
-    color: "black",   
+  const innerBoxEducationEqual12 = {
+    backgroundColor: "#0af502",
+    color: "black",
+    borderRadius: "5px",
+    boxShadow: 3,
     cursor: "pointer",
-    display: "block",
+    display: "flex",
     justifyContent: "center",
     py: 2,
     px: 2,
     width: 150,
     m: 1,
     direction: "rtl",
-    
+    flexDirection: "column",
   };
+
+  const innerBoxEducationUpper12 = {
+    backgroundColor: "#e1f502",
+    color: "black",
+    borderRadius: "5px",
+    boxShadow: 3,
+    cursor: "pointer",
+    display: "flex",
+    justifyContent: "center",
+    py: 2,
+    px: 2,
+    width: 150,
+    m: 1,
+    direction: "rtl",
+    flexDirection: "column",
+  };
+
+  const checkEducation = (education_level: any) => {
+    let educationNumber = Number(education_level);
+
+    if (educationNumber === 12) {
+      return innerBoxEducationEqual12;
+    } else if (educationNumber > 12) {
+      return innerBoxEducationUpper12;
+    } else {
+      return innerBoxEducationUnder12;
+    }
+  };
+
 
   return (
     <>
       {details?.map((detail, index: number) => (
-        <Grid item xs={12} md={4} lg={4} >
-
-          <Box sx={innerBox}>
-          
-            {/* <Box>{Face3OutlinedIcon}</Box> */}
-            <Box > {detail.class_rome_name} </Box>
-
-            <Box >
+        <Grid item xs={12} md={4} lg={4}>
+          <Box sx={checkEducation(detail.education_level)}>
+            <Box>
               {" "}
               {detail.start_time}- {detail.end_time}{" "}
             </Box>
-            <Box > {detail.course_name}</Box>
-            <Box > {detail.name} </Box>
-            <Box >{detail.lesson_name}</Box>
-            <Box >{detail.teacher_name}</Box>            
-            <Box >{detail.gender}</Box>
-            <Box >{detail.education_level}</Box>
+
+            <Box> {detail.class_rome_name} </Box>
+            <Box> {detail.course_name}</Box>
+            <Box> {detail.name} </Box>
+            <Box>{detail.lesson_name}</Box>
+            <Box>{detail.teacher_name}</Box>
+            <Box>
+              <img
+                src={detail.gender === "female" ? femalepng : malepng}
+                alt=""
+                width={30}
+              />
+            </Box>
+            {/* <Box >{detail.education_level}</Box> */}
           </Box>
         </Grid>
       ))}
